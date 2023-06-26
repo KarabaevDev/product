5d13733a0f2d459eaf1ee0102553e45ab31560a3
KarabaevDev <saidmurodkarabaev@gmail.com>
2023-06-26 14:11:34 +0600

    end

--- js/script.js
+++ js/script.js
@@ -1,17 +1,64 @@
-new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    centeredSlides: true,
+// new Swiper(".mySwiper", {
+//     slidesPerView: 3,
+//     spaceBetween: 30,
+//     centeredSlides: true,
+//     autoplay: {
+//         delay: 2500,
+//         disableOnInteraction: false,
+//     },
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+// });
+
+let swiper = new Swiper(".mySwiper", {
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
     },
+    slidesPerView: 1,
+    spaceBetween: 10,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        375: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        400: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        450: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1440: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
     },
 });
